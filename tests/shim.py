b05fa2d20d1936d69a03a0caa5bf27f4d538ba70
fancidev <fancidev@gmail.com>
2022-09-05 20:16:34 +0800

    Add test_clicked.py to test signal-slot connection.

--- tests/shim.py
+++ tests/shim.py
@@ -9,16 +9,16 @@ __all__ = 'QtCore',
 qt_module_name = os.getenv("TEST_QT_MODULE", "").lower()
 
 if qt_module_name == "pyqt5":
-    from PyQt5 import QtCore
+    from PyQt5 import QtCore, QtWidgets
 
 elif qt_module_name == "pyqt6":
-    from PyQt6 import QtCore
+    from PyQt6 import QtCore, QtWidgets
 
 elif qt_module_name == "pyside2":
-    from PySide2 import QtCore
+    from PySide2 import QtCore, QtWidgets
 
 elif qt_module_name == "pyside6":
-    from PySide6 import QtCore
+    from PySide6 import QtCore, QtWidgets
 
 else:
     raise ImportError(f"unsupported TEST_QT_MODULE: '{qt_module_name}'")
---
+++ tests/test_clicked.py
@@ -0,0 +1,26 @@
+""" test_clicked.py - test asyncSlot with QAbstractButton """
+
+import asyncio
+from shim import QtCore, QtWidgets
+from asyncslot import asyncSlot, AsyncSlotRunner
+
+
+async def quit_later():
+    await asyncio.sleep(0)
+    QtWidgets.QApplication.quit()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+
+    button = QtWidgets.QPushButton()
+    button.setText('Quit')
+    button.clicked.connect(asyncSlot(quit_later))
+    button.show()
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(button.click)
+    timer.start()
+
+    with AsyncSlotRunner():
+        app.exec()
