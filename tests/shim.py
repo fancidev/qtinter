b10f04266bf68d6dd19bb44fe5a17bd6a93e6654
fancidev <fancidev@gmail.com>
2022-09-07 17:40:35 +0800

    Import QtWidgets in test shim.

--- tests/shim.py
+++ tests/shim.py
@@ -6,23 +6,26 @@ import os
 __all__ = 'QtCore',
 
 
-qt_module_name = os.getenv("TEST_QT_MODULE", "").lower()
+qt_module_name = os.getenv("TEST_QT_MODULE", "")
+if qt_module_name == "":
+    raise ImportError("environment variable TEST_QT_MODULE must be set")
 
-if qt_module_name == "pyqt5":
-    from PyQt5 import QtCore
+
+if qt_module_name == "PyQt5":
+    from PyQt5 import QtCore, QtWidgets
     QtCore.Signal = QtCore.pyqtSignal
     QtCore.Slot = QtCore.pyqtSlot
 
-elif qt_module_name == "pyqt6":
-    from PyQt6 import QtCore
+elif qt_module_name == "PyQt6":
+    from PyQt6 import QtCore, QtWidgets
     QtCore.Signal = QtCore.pyqtSignal
     QtCore.Slot = QtCore.pyqtSlot
 
-elif qt_module_name == "pyside2":
-    from PySide2 import QtCore
+elif qt_module_name == "PySide2":
+    from PySide2 import QtCore, QtWidgets
 
-elif qt_module_name == "pyside6":
-    from PySide6 import QtCore
+elif qt_module_name == "PySide6":
+    from PySide6 import QtCore, QtWidgets
 
 else:
-    raise ImportError(f"unsupported TEST_QT_MODULE: '{qt_module_name}'")
+    raise ImportError(f"unsupported TEST_QT_MODULE value: '{qt_module_name}'")
