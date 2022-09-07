7a8f8ef02cbf9ad733e1e702b6878ed73dfbab97
fancidev <fancidev@gmail.com>
2022-09-07 17:10:34 +0800

    Rework test_sleep.py.

--- tests/test_sleep.py
+++ tests/test_sleep.py
@@ -10,28 +10,36 @@ class TestSleep(unittest.TestCase):
     def setUp(self) -> None:
         if QtCore.QCoreApplication.instance() is None:
             self.app = QtCore.QCoreApplication([])
+        else:
+            self.app = QtCore.QCoreApplication.instance()
         self.loop = asyncslot.AsyncSlotDefaultEventLoop()
 
     def tearDown(self) -> None:
         self.loop.close()
+        self.app = None
 
     def test_sleep(self):
-        total_time = 0.0
+        total_duration = 2.0
 
-        async def sleep_for(times, duration):
+        async def sleep_for(times):
             for _ in range(times):
-                await asyncio.sleep(duration)
+                await asyncio.sleep(total_duration / times)
 
         async def entry():
-            nonlocal total_time
             t1 = time.time()
-            await asyncio.gather(sleep_for(4, 1.0), sleep_for(2, 2.0))
+            tasks = []
+            for _ in range(100):
+                tasks.append(sleep_for(8))
+                tasks.append(sleep_for(4))
+                tasks.append(sleep_for(2))
+                tasks.append(sleep_for(1))
+            await asyncio.gather(*tasks)
             t2 = time.time()
-            total_time = t2 - t1
+            return t2 - t1
 
-        self.loop.run_until_complete(entry())
-        self.assertGreater(total_time, 3.5)
-        self.assertLess(total_time, 4.5)
+        total_time = self.loop.run_until_complete(entry())
+        self.assertGreater(total_time, total_duration - 0.1)
+        self.assertLess(total_time, total_duration + 1.0)
 
 
 if __name__ == '__main__':
