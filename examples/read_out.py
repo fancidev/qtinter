1134520ed65705fc869462ca8a169beaebbcf2a0
fancidev <fancidev@gmail.com>
2022-10-02 19:25:37 +0800

    Add read_out example.

---
+++ examples/read_out.py
@@ -0,0 +1,73 @@
+"""Read out input using QTextToSpeech"""
+
+import asyncio
+import getopt
+import qtinter
+import sys
+from PyQt5 import QtGui, QtTextToSpeech
+
+
+async def read_out(engine: QtTextToSpeech.QTextToSpeech, echo=False):
+    while True:
+        try:
+            line = await asyncio.get_running_loop().run_in_executor(None, input)
+        except EOFError:
+            break
+        if echo:
+            print(line)
+        engine.say(line)
+        if engine.state() == QtTextToSpeech.QTextToSpeech.State.Speaking:
+            # If the line contains no speakable content, state remains Ready.
+            state = await qtinter.asyncsignal(engine.stateChanged)
+            assert state == QtTextToSpeech.QTextToSpeech.State.Ready
+
+
+def main():
+    app = QtGui.QGuiApplication([])
+
+    engine = QtTextToSpeech.QTextToSpeech()
+    locales = dict((l.name(), l) for l in engine.availableLocales())
+    voices = dict((v.name(), v) for v in engine.availableVoices())
+    usage = (f"{sys.argv[0]} [-e] [-l locale] [-p pitch] [-r rate] [-v voice]\n"
+             f"Read out text from stdin."
+             f"Options:\n"
+             f"    -e          Echo each line before reading it out\n"
+             f"    -l locale   One of {', '.join(sorted(locales))}\n"
+             f"                Default: {engine.locale().name()}\n"
+             f"    -p pitch    Number between -1.0 and +1.0 (default: 0.0)\n"
+             f"    -r rate     Number between -1.0 and +1.0 (default: 0.0)\n"
+             f"    -v voice    One of {', '.join(sorted(voices))}\n"
+             f"                Default: {engine.voice().name()}\n")
+
+    try:
+        args, rest = getopt.getopt(sys.argv[1:], "el:p:r:v:")
+    except getopt.error:
+        print(usage, file=sys.stderr)
+        return 1
+
+    if rest:
+        print(usage, file=sys.stderr)
+        return 1
+
+    echo = False
+    for opt, val in args:
+        if opt == "-e":
+            echo = True
+        elif opt == "-l":
+            engine.setLocale(locales[val])
+        elif opt == "-p":
+            engine.setPitch(float(val))
+        elif opt == "-r":
+            engine.setRate(float(val))
+        elif opt == "-v":
+            engine.setVoice(voices[val])
+        else:
+            print(usage, file=sys.stderr)
+            return 1
+
+    with qtinter.using_qt_from_asyncio():
+        asyncio.run(read_out(engine, echo))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
