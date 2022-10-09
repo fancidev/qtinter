dac05c8cf1c681e4bfeff3cc7a6c1927196217d0
fancidev <fancidev@gmail.com>
2022-10-09 21:44:09 +0800

    Add example hit_100.py to demo modal().

---
+++ examples/hit_100.py
@@ -0,0 +1,30 @@
+"""Demo non-blocking dialog exec"""
+
+import asyncio
+import qtinter
+from PyQt6 import QtWidgets
+
+
+async def main():
+    async def counter():
+        nonlocal n
+        while True:
+            print(f"\r{n}", end='', flush=True)
+            await asyncio.sleep(0.025)
+            n += 1
+
+    n = 0
+    counter_task = asyncio.create_task(counter())
+    await qtinter.modal(QtWidgets.QMessageBox.information)(
+        None, "Hit 100", "Click OK when you think you hit 100.")
+    counter_task.cancel()
+    if n == 100:
+        print("\nYou did it!")
+    else:
+        print(f"\nTry again!")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    with qtinter.using_qt_from_asyncio():
+        asyncio.run(main())
