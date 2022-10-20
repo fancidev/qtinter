cb516648ae29b139c443e3c4bde0b7717b69983d
fancidev <fancidev@gmail.com>
2022-10-20 21:54:22 +0800

    Fix script path in test case.

--- tests/example_tests.py
+++ tests/example_tests.py
@@ -1,15 +1,20 @@
 """Run the examples"""
 
+import os
 import unittest
 from test.support.script_helper import run_python_until_end
+
+
+folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 class TestExamples(unittest.TestCase):
 
     def test_where_am_i(self):
         result, cmd = run_python_until_end(
-            "../examples/where_am_i.py",
-            PYTHONPATH="../src")
+            os.path.join("examples", "where_am_i.py"),
+            __cwd=folder,
+            PYTHONPATH="src")
         if result.rc != 0:
             result.fail(cmd)
         else:
