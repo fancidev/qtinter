80ab4faaecd1c1a7b02e9751eb9904bd32b110b7
fancidev <fancidev@gmail.com>
2022-10-28 21:14:33 +0800

    Improve binding tests.

--- tests/binding_tests.py
+++ tests/binding_tests.py
@@ -2,6 +2,7 @@
 
 import importlib
 import os
+import sys
 import unittest
 
 
@@ -18,6 +19,10 @@ else:
 qc = QtCore.Qt.ConnectionType.QueuedConnection
 
 
+class SenderObject(QtCore.QObject):
+    signal = Signal(bool)
+
+
 called = []
 
 
@@ -30,11 +35,43 @@ def visit(s, tag=None):
     called.append(msg)
 
 
-class MySignalObject(QtCore.QObject):
-    ready = Signal(bool)
+def _test_slot(slot):
+    called.clear()
+    sender = SenderObject()
+    sender.signal.connect(slot)
+    sender.signal.emit(True)
+    return called.copy()
 
 
-class MySlotNonObject:
+# -----------------------------------------------------------------------------
+# Tests on free function slots
+# -----------------------------------------------------------------------------
+
+def func():
+    visit('func')
+
+
+@Slot()
+def slot_func():
+    visit('slot_func')
+
+
+class TestFreeFunction(unittest.TestCase):
+
+    def test_func(self):
+        result = _test_slot(func)
+        self.assertEqual(result, ['func'])
+
+    def test_slot_func(self):
+        result = _test_slot(slot_func)
+        self.assertEqual(result, ['slot_func'])
+
+
+# -----------------------------------------------------------------------------
+# Tests on method slots
+# -----------------------------------------------------------------------------
+
+class Receiver:
     secret = 'Cls'
 
     def __init__(self):
@@ -58,10 +95,8 @@ class MySlotNonObject:
         def slot_class_method(cls):
             visit('slot_class_method', cls)
     except AttributeError:
-        if is_pyqt:
-            pass
-        else:
-            raise
+        # This construct is not supported on PyQt below Python 3.10.
+        pass
 
     @classmethod
     @Slot()
@@ -78,10 +113,8 @@ class MySlotNonObject:
         def slot_static_method():
             visit('slot_static_method')
     except AttributeError:
-        if is_pyqt:
-            pass
-        else:
-            raise
+        # This construct is not supported on PyQt below Python 3.10.
+        pass
 
     @staticmethod
     @Slot()
@@ -89,42 +122,14 @@ class MySlotNonObject:
         visit('static_slot_method')
 
 
-class MySlotObject(MySlotNonObject, QtCore.QObject):
+class ReceiverObject(Receiver, QtCore.QObject):
     pass
 
 
-def func():
-    visit('func')
-
-
-@Slot()
-def slot_func():
-    visit('slot_func')
-
-
-def _test_slot(slot):
-    called.clear()
-    sender = MySignalObject()
-    sender.ready.connect(slot)
-    sender.ready.emit(True)
-    return called.copy()
-
-
-class TestSlotOnFreeFunction(unittest.TestCase):
-
-    def test_func(self):
-        result = _test_slot(func)
-        self.assertEqual(result, ['func'])
-
-    def test_slot_func(self):
-        result = _test_slot(slot_func)
-        self.assertEqual(result, ['slot_func'])
-
-
-class TestSlotOnQObject(unittest.TestCase):
+class TestReceiverObject(unittest.TestCase):
 
     def setUp(self):
-        self.receiver = MySlotObject()
+        self.receiver = ReceiverObject()
 
     def tearDown(self):
         self.receiver = None
@@ -142,7 +147,7 @@ class TestSlotOnQObject(unittest.TestCase):
         self.assertEqual(result, ['class_method(Cls)'])
 
     def test_slot_class_method(self):
-        if is_pyqt:
+        if is_pyqt and sys.version_info < (3, 10):
             # PyQt does not support such construct.
             self.assertFalse(hasattr(self.receiver, "slot_class_method"))
         else:
@@ -163,7 +168,7 @@ class TestSlotOnQObject(unittest.TestCase):
         self.assertEqual(result, ['static_method'])
 
     def test_slot_static_method(self):
-        if is_pyqt:
+        if is_pyqt and sys.version_info < (3, 10):
             # PyQt does not support such construct.
             self.assertFalse(hasattr(self.receiver, "slot_static_method"))
         else:
@@ -175,10 +180,10 @@ class TestSlotOnQObject(unittest.TestCase):
         self.assertEqual(result, ['static_slot_method'])
 
 
-class TestSlotOnNonQObject(TestSlotOnQObject):
+class TestReceiver(TestReceiverObject):
     def setUp(self):
         super().setUp()
-        self.receiver = MySlotNonObject()
+        self.receiver = Receiver()
 
     def test_slot_method(self):
         if is_pyqt:
@@ -189,125 +194,125 @@ class TestSlotOnNonQObject(TestSlotOnQObject):
             super().test_slot_method()
 
 
-# class Sender(QtCore.QObject):
-#     signal = Signal(int)
-#
-#
-# class Receiver:
-#     def __init__(self, output):
-#         self.output = output
-#
-#     async def original_slot(self, v):
-#         self.output[0] += v
-#
-#
-# class StrongReceiver:
-#     __slots__ = 'output',
-#
-#     def __init__(self, output):
-#         self.output = output
-#
-#     def method(self, v):
-#         self.output[0] -= v
-#
-#
-# class Control(QtCore.QObject):
-#     valueChanged = Signal((int,), (str,))
-#
-#
-# class Widget(QtCore.QObject):
-#     def __init__(self):
-#         super().__init__()
-#         self.control1 = Control(self)
-#         self.control1.setObjectName("control1")
-#         self.control2 = Control(self)
-#         self.control2.setObjectName("control2")
-#         self.control3 = Control(self)
-#         self.control3.setObjectName("control3")
-#         self.control4 = Control(self)
-#         self.control4.setObjectName("control4")
-#         self.metaObject().connectSlotsByName(self)
-#         self.values = []
-#
-#     def on_control1_valueChanged(self, newValue):
-#         self.values.append("control1")
-#         self.values.append(newValue)
-#
-#     @Slot(int)
-#     def on_control2_valueChanged(self, newValue):
-#         self.values.append("control2")
-#         self.values.append(newValue)
-#
-#     @asyncslot
-#     async def on_control3_valueChanged(self, newValue):
-#         self.values.append("control3")
-#         self.values.append(newValue)
-#
-#     @asyncslot
-#     @Slot(str)
-#     async def on_control4_valueChanged(self, newValue):
-#         self.values.append("control4")
-#         self.values.append(newValue)
-#
-#
-# class TestSlotSelection(unittest.TestCase):
-#     def setUp(self) -> None:
-#         if QtCore.QCoreApplication.instance() is not None:
-#             self.app = QtCore.QCoreApplication.instance()
-#         else:
-#             self.app = QtCore.QCoreApplication([])
-#
-#     def tearDown(self) -> None:
-#         self.app = None
-#
-#     def test_decorated(self):
-#         values1 = []
-#         values2 = []
-#         values3 = []
-#         values4 = []
-#
-#         def callback():
-#             w = Widget()
-#
-#             w.values.clear()
-#             w.control1.valueChanged[int].emit(12)
-#             w.control1.valueChanged[str].emit('ha')
-#             values1[:] = w.values
-#
-#             w.values.clear()
-#             w.control2.valueChanged[int].emit(12)
-#             w.control2.valueChanged[str].emit('ha')
-#             values2[:] = w.values
-#
-#             w.values.clear()
-#             w.control3.valueChanged[int].emit(12)
-#             w.control3.valueChanged[str].emit('ha')
-#             values3[:] = w.values
-#
-#             w.values.clear()
-#             w.control4.valueChanged[int].emit(12)
-#             w.control4.valueChanged[str].emit('ha')
-#             values4[:] = w.values
-#
-#             self.app.quit()
-#
-#         with using_asyncio_from_qt():
-#             QtCore.QTimer.singleShot(0, callback)
-#             if hasattr(self.app, "exec"):
-#                 self.app.exec()
-#             else:
-#                 self.app.exec_()
-#
-#         if is_pyqt:
-#             self.assertEqual(values1, ["control1", 12, "control1", "ha"])
-#         else:
-#             self.assertEqual(values1, [])
-#         self.assertEqual(values2, ["control2", 12])
-#         if is_pyqt:
-#             self.assertEqual(values3, ["control3", 12, "control3", "ha"])
-#         else:
-#             self.assertEqual(values3, [])
-#         self.assertEqual(values4, ["control4", "ha"])
+# -----------------------------------------------------------------------------
+# Tests on receiver object without __weakref__ slot
+# -----------------------------------------------------------------------------
+
+class StrongReceiver:
+    __slots__ = ()
+
+    def method(self):
+        called.append('special')
+
+
+class StrongReceiverObject(StrongReceiver, QtCore.QObject):
+    __slots__ = ()
+
+
+class TestStrongReceiverObject(unittest.TestCase):
+    def setUp(self):
+        self.receiver = StrongReceiverObject()
+
+    def tearDown(self):
+        self.receiver = None
+
+    def test_method(self):
+        result = _test_slot(self.receiver.method)
+        self.assertEqual(result, ['special'])
+
+
+class TestStrongReceiver(TestStrongReceiverObject):
+    def setUp(self):
+        super().setUp()
+        self.receiver = StrongReceiver()
+
+    def test_method(self):
+        with self.assertRaises(SystemError if is_pyqt else TypeError):
+            _test_slot(self.receiver.method)
+
+
+# -----------------------------------------------------------------------------
+# Tests on multiple signature for same signal name
+# -----------------------------------------------------------------------------
+
+class Control(QtCore.QObject):
+    valueChanged = Signal((int,), (str,))
+
+
+class Widget(QtCore.QObject):
+    def __init__(self):
+        super().__init__()
+        self.control1 = Control(self)
+        self.control1.setObjectName("control1")
+        self.control2 = Control(self)
+        self.control2.setObjectName("control2")
+        self.control3 = Control(self)
+        self.control3.setObjectName("control3")
+        self.control4 = Control(self)
+        self.control4.setObjectName("control4")
+        self.metaObject().connectSlotsByName(self)
+        self.values = []
+
+    def on_control1_valueChanged(self, newValue):
+        self.values.append("control1")
+        self.values.append(newValue)
+
+    @Slot(int)
+    def on_control2_valueChanged(self, newValue):
+        self.values.append("control2")
+        self.values.append(newValue)
+
+    @Slot(str)
+    def on_control3_valueChanged(self, newValue):
+        self.values.append("control3")
+        self.values.append(newValue)
+
+    @Slot(int)
+    @Slot(str)
+    def on_control4_valueChanged(self, newValue):
+        self.values.append("control4")
+        self.values.append(newValue)
+
+
+class TestSlotSelection(unittest.TestCase):
+    def test_slot_selection(self):
+        values1 = []
+        values2 = []
+        values3 = []
+        values4 = []
+
+        def callback():
+            w = Widget()
+
+            w.values.clear()
+            w.control1.valueChanged[int].emit(12)
+            w.control1.valueChanged[str].emit('ha')
+            values1[:] = w.values
+
+            w.values.clear()
+            w.control2.valueChanged[int].emit(12)
+            w.control2.valueChanged[str].emit('ha')
+            values2[:] = w.values
+
+            w.values.clear()
+            w.control3.valueChanged[int].emit(12)
+            w.control3.valueChanged[str].emit('ha')
+            values3[:] = w.values
+
+            w.values.clear()
+            w.control4.valueChanged[int].emit(12)
+            w.control4.valueChanged[str].emit('ha')
+            values4[:] = w.values
+
+        callback()
+
+        if is_pyqt:
+            self.assertEqual(values1, ["control1", 12, "control1", "ha"])
+        else:
+            self.assertEqual(values1, [])
+        self.assertEqual(values2, ["control2", 12])
+        self.assertEqual(values3, ["control3", "ha"])
+        self.assertEqual(values4, ["control4", 12, "control4", "ha"])
 
 
 if __name__ == '__main__':
